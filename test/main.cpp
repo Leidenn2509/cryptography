f87ce3f7074096a1aaf088b994cb5729d202f69b
Alexander Raish <leidenn2509@gmail.com>
2019-09-24 19:30:40 +0700

    fix fast_pow

--- test/main.cpp
+++ test/main.cpp
@@ -3,7 +3,8 @@
 
 uint64_t fast_pow(uint64_t base, uint64_t power, uint64_t p = std::numeric_limits<uint64_t>::max()) {
     uint64_t res = 1;
-    for(uint64_t i = 0; i < std::log2(power) + 1; ++i) {
+    uint64_t t = std::log2(power) + 1;
+    for(uint64_t i = 0; i < t; ++i) {
         if (power % 2 == 1) {
             res = std::fmod(res*base, p);
         }
